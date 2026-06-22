5e5762885aa702b87e96c1a2963a48bb3a22148e
ehughes <ehughes@wavenumber.net>
2026-06-22 10:31:57 -0400

    Release altium-cruncher 2026.6.22
    Release altium-cruncher 2026.6.22 with altium-monkey 2026.6.21 and PR hygiene CI.

--- src/py/altium_cruncher/_version.py
+++ src/py/altium_cruncher/_version.py
@@ -8,7 +8,7 @@ from datetime import date
 from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as distribution_version
 
-__version__ = "2026.6.17"
+__version__ = "2026.6.22"
 
 _DISTRIBUTION_NAME = "altium-cruncher"
 _CONTROLLED_DEPENDENCIES = (
